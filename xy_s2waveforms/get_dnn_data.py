e5bc721fdbeec4d51ee5a068712aad87bac878ed
dbarg <derek.barge@fysik.su.se>
2018-05-04 11:26:39 +0200

    work in progress

--- xy_s2waveforms/get_dnn_data.py
+++ xy_s2waveforms/get_dnn_data.py
@@ -84,9 +84,10 @@ def get_data(dir_input_s2, df_events, s2_window_max, resample_factor=1):
 
     for iEvent, event_num in enumerate(lst_events):
         
-        gc.collect()
-        
         t0 = time.time()
+        
+        #gc.collect() # time consuming
+        
         
         ################################################################################################
         # Get Event Information
@@ -107,6 +108,10 @@ def get_data(dir_input_s2, df_events, s2_window_max, resample_factor=1):
         x_true          = df_event.loc['x'              ]
         y_true          = df_event.loc['y'              ]
     
+        event_train_truth[0]   = x_true
+        event_train_truth[1]   = y_true
+        train_truth[iEvent, :] = event_train_truth
+
         
         ################################################################################################
         ################################################################################################
@@ -132,47 +137,54 @@ def get_data(dir_input_s2, df_events, s2_window_max, resample_factor=1):
             df_s2waveforms = waveform_utils.addEmptyChannelsToDataFrame(df_s2waveforms)
             f.close()
     
-
+        t1 = time.time()
+        
+        
         ################################################################################################
         ################################################################################################
         
-        
-        for iChannel, row in df_s2waveforms.iterrows():
+        for iChannel, ser_channel in df_s2waveforms.iterrows():
             
             ############################################################################################
+            # This piece takes ~ 0.7 s
             ############################################################################################
     
             arr_channel_padded = get_padded_array(
-                pd.Series.copy(row, deep=True),
+                ser_channel,
                 s2_window_max,
                 event_s2_left,
                 event_s2_right,
                 event_s2_length)
-               
+          
             
             ############################################################################################
-            # memory grows here
+            # This piece takes ~ 0.3 s
             ############################################################################################
             
-            i0 = iChannel * s2_window_max
-            i1 = i0       + s2_window_max
+            if (not resample):
+            
+                ########################################################################################
+                ########################################################################################
+            
+                i0 = iChannel * s2_window_max
+                i1 = i0       + s2_window_max
+    
+                #arr_temp[i0:i1] = np.array(arr_channel_padded, copy=True)
+                arr_temp[i0:i1] = arr_channel_padded
 
-            arr_temp[i0:i1] = np.array(arr_channel_padded, copy=True)
+            else:
             
-            
-            ############################################################################################
-            ############################################################################################
-            
-            if (resample):
+                ########################################################################################
+                ########################################################################################
                 
                 arr_resampled = skimgmeas.block_reduce(arr_channel_padded, block_size=(resample_factor,), func=np.sum)
                 
                 i0_r = iChannel * s2_window_max_resampled
                 i1_r = i0_r     + s2_window_max_resampled
                
-                arr_temp_resampled[i0_r : i1_r] = np.array(arr_resampled, copy=True)
+                #arr_temp_resampled[i0_r : i1_r] = np.array(arr_resampled, copy=True)
+                arr_temp_resampled[i0_r : i1_r] = arr_resampled
 
-            
                 #print("Resampled by factor: " + str(resample_factor))
                 #print(arr_resampled.shape)
                 #print(train_data_resampled.shape)            
@@ -180,31 +192,40 @@ def get_data(dir_input_s2, df_events, s2_window_max, resample_factor=1):
                 
                 
             ############################################################################################
+            # End loop over channels
             ############################################################################################
 
             continue
     
     
         ################################################################################################
-        # End loop over channels
         ################################################################################################
+        
+        t2 = time.time()
         
         #t1 = time.time()
         #performance_utils.time_event(iEvent, event_num, t1 - t0)
         
         
         ################################################################################################
-        # memory grows here
+        # *** Memory grows here ***
         ################################################################################################
         
         #continue
         
-        assert(train_data.shape[1] == arr_temp.size)
-        
-        train_data[iEvent]           = np.array(arr_temp          , copy=True)
-        train_data_resampled[iEvent] = np.array(arr_temp_resampled, copy=True)
-        
+        if (not resample):
+            
+            #train_data[iEvent] = np.array(arr_temp, copy=True)
+            train_data[iEvent] = arr_temp
+            
+        else:
+            
+            #train_data_resampled[iEvent] = np.array(arr_temp_resampled, copy=True)
+            train_data_resampled[iEvent] = arr_temp_resampled
+            
         #continue
+        
+        t3 = time.time()
         
         
         ################################################################################################
@@ -227,20 +248,15 @@ def get_data(dir_input_s2, df_events, s2_window_max, resample_factor=1):
             
             assert(arr_s2integrals_equal)
         
-        
-        ################################################################################################
-        ################################################################################################
-        
-        event_train_truth[0]      = x_true
-        event_train_truth[1]      = y_true
-        train_truth[iEvent, :]    = event_train_truth
-        
-        #print("x, y: " + str(x_true) + ", " + str(y_true))
+        t4 = time.time()
         
         
         ################################################################################################
         # Sanity
         ################################################################################################
+        
+        #assert(train_data.shape[1] == arr_temp.size)
+        assert(train_data.shape[1] == s2_window_max*n_channels)
         
         df_event_s2s  = df_s2waveforms[df_s2waveforms['left'] != 0].copy(deep=True)
         df_event_s2s.reset_index(inplace=True, drop=True)
@@ -271,10 +287,30 @@ def get_data(dir_input_s2, df_events, s2_window_max, resample_factor=1):
         ################################################################################################
         # End loop over events
         ################################################################################################
-    
-        t1 = time.time()
-    
-        performance_utils.time_event(iEvent, event_num, t1 - t0)
+
+        t5   = time.time()
+        dt54 = round(t5 - t4, 2)
+        dt43 = round(t4 - t3, 2)
+        dt32 = round(t3 - t2, 2)
+        dt21 = round(t2 - t1, 2)
+        dt10 = round(t1 - t0, 2)
+        dt50 = round(t5 - t0, 2)
+        
+        performance_utils.time_event(iEvent, event_num, t5 - t0)
+
+        if (False):
+            
+            print(" -> IO:           " + str(dt10))
+            print(" -> channel loop: " + str(dt21))
+            print(" -> array assign: " + str(dt32))
+            print(" -> resampling:   " + str(dt43))
+            print(" -> Sanity:       " + str(dt54))
+            print(" -> Total:        " + str(dt50))
+            print()
+        
+        
+        ################################################################################################
+        ################################################################################################
         
         continue
 
@@ -363,10 +399,9 @@ def get_padded_array(
     # Pad to the widest S2 over all events
     ############################################################################################
         
-    arr_channel_padded                       = np.zeros(s2_window_max, dtype ='float32')
     arr_channel_padded[0 : arr_channel.size] = arr_channel
     
-    return np.array(arr_channel_padded)
+    return arr_channel_padded
 
 
 ####################################################################################################
