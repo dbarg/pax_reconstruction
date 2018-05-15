d81e91cb2adc5d48fabb8eef4cb44eeeb97956f5
Derek Barge <dbarge@midway-login1.rcc.local>
2018-05-15 04:14:50 -0500

    work in progress

---
+++ gpu/theano/gpu_test.py
@@ -0,0 +1,46 @@
+####################################################################################################
+####################################################################################################
+
+import os
+import sys
+import time
+
+import numpy
+import pygpu
+import theano
+
+from theano import config
+from theano import function
+from theano import shared
+from theano import tensor
+
+print("Python Version: " + sys.version)
+
+pygpu.test()
+theano.test()
+
+exit(0)
+
+
+####################################################################################################
+####################################################################################################
+
+vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
+iters = 1000
+
+rng = numpy.random.RandomState(22)
+x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
+f = function([], tensor.exp(x))
+print(f.maker.fgraph.toposort())
+t0 = time.time()
+for i in range(iters):
+    r = f()
+t1 = time.time()
+print("Looping %d times took %f seconds" % (iters, t1 - t0))
+print("Result is %s" % (r,))
+if numpy.any([isinstance(x.op, tensor.Elemwise) and
+              ('Gpu' not in type(x.op).__name__)
+              for x in f.maker.fgraph.toposort()]):
+    print('Used the cpu')
+else:
+    print('Used the gpu')
