f579f5549f196a1f8086e2329446d245c7caad27
dbarg <derek.barge@fysik.su.su>
2019-09-16 10:38:27 -0500

    change comment header style

--- gpu/theano/gpu_test.py
+++ gpu/theano/gpu_test.py
@@ -1,29 +1,35 @@
-####################################################################################################
-####################################################################################################
 
-import os
-import sys
-import time
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
+import keras
 import numpy
+import os
 import pygpu
+import sys
 import theano
+import time
 
 from theano import config
 from theano import function
 from theano import shared
 from theano import tensor
 
-print("Python Version: " + sys.version)
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+print("Python Version: {0}".format(sys.version))
+print("Theano Version: {0}".format(theano.version))
+print("Keras Version:  {0}".format(keras.version))
 
 pygpu.test()
 theano.test()
 
-exit(0)
 
-
-####################################################################################################
-####################################################################################################
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
 iters = 1000
@@ -31,16 +37,24 @@ iters = 1000
 rng = numpy.random.RandomState(22)
 x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
 f = function([], tensor.exp(x))
-print(f.maker.fgraph.toposort())
 t0 = time.time()
+
 for i in range(iters):
     r = f()
+    continue
+    
 t1 = time.time()
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+print(f.maker.fgraph.toposort())
 print("Looping %d times took %f seconds" % (iters, t1 - t0))
 print("Result is %s" % (r,))
-if numpy.any([isinstance(x.op, tensor.Elemwise) and
-              ('Gpu' not in type(x.op).__name__)
-              for x in f.maker.fgraph.toposort()]):
+
+if numpy.any([isinstance(x.op, tensor.Elemwise) and ('Gpu' not in type(x.op).__name__) for x in f.maker.fgraph.toposort()]):
     print('Used the cpu')
+    
 else:
     print('Used the gpu')
