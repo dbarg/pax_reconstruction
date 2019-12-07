fe51f76b87d7321db866bb785460e9a195e2aa4d
dbarg <derek.barge@fysik.su.su>
2019-12-07 16:28:27 -0600

    work in progress

--- sbatch_cpu/s2waveforms_xy_ts0200.sh
+++ sbatch_cpu/s2waveforms_xy_ts0200.sh
@@ -12,11 +12,11 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=500MB
 
-#SBATCH --output=log_s2waveforms_xy_ts125.txt # output log file
-#SBATCH --error=log_s2waveforms_xy_ts125.txt  # error file
+#SBATCH --output=log_s2waveforms_xy_ts200.txt # output log file
+#SBATCH --error=log_s2waveforms_xy_ts200.txt  # error file
 
 source ~/.bash/.setup_ml.sh
 
-srun python ../nn_s2waveforms_xy_train.py -directory /project2/lgrandi/dbarge/pax_merge/temp_s2/ -max_dirs 22 -events_per_batch 100 -epochs 10 -downsample 8
+srun python ../nn_s2waveforms_xy_train.py -directory /project2/lgrandi/dbarge/pax_merge/temp_s2/ -max_dirs 22 -events_per_batch 100 -epochs 10 -downsample 5
 
 echo "Done"
