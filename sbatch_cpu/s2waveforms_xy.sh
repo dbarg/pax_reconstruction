9e5e1dbc26c09472a554bd58dcf8aefca9116c1f
dbarg <derek.barge@fysik.su.su>
2019-12-06 21:36:34 -0600

    work in progress

--- sbatch_cpu/s2waveforms_xy.sh
+++ sbatch_cpu/s2waveforms_xy.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
 #SBATCH --job-name=nn_cpu
-#SBATCH --time=48:00:00
-
-#SBATCH --account=pi-lgrandi
-#SBATCH --partition=xenon1t,dali
-#SBATCH --qos=xenon1t
+#SBATCH --time=24:00:00
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=64GB
-
-##SBATCH --partition=bigmem2
-##SBATCH --mem=128GB
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=32GB
 
 #SBATCH --output=log_s2waveforms_xy.txt # output log file
 #SBATCH --error=log_s2waveforms_xy.txt  # error file
 
 source ~/.bash/.setup_ml.sh
 
-echo "Starting..."
-
-srun python ../nn_s2waveforms_xy_train.py -directory /project2/lgrandi/dbarge/pax_merge/temp_s2/ -max_dirs 2 -events_per_batch 100 -downsample 1
+srun python ../nn_s2waveforms_xy_train.py -directory /project2/lgrandi/dbarge/pax_merge/temp_s2/ -max_dirs 2 -events_per_batch 10 -downsample 25
 
 # Add lines here to run your GPU-based computations.
 echo "Done"
